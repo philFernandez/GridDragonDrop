80ac4ad3bf771ee541631069e35718cb0ab98bfa
philFernandez <philfernandez@protonmail.com>
2023-02-28 15:56:22 -0800

    secondski

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,30 +1,28 @@
 import '../css/style.css';
 
-const GRID_ROWS = 20;
-const GRID_COLUMNS = 25;
 
-/**
- * Each row spans 2 grid rows
- * and each columns spans 5 grid columns
- */
-const ROWS = 14;
-const COLUMNS = 5;
+function gridTheBoxes(nRows: number, rowSpan: number) {
+    const boxes = document.querySelectorAll('div.box') as NodeListOf<HTMLDivElement>;
 
-const ROW_SPAN = 2;
-
-
-const boxes = document.querySelectorAll('div.box') as NodeListOf<HTMLDivElement>;
-
-
-
-for (let box of boxes) {
-    let gridRowIndex = 1;
-
-    for (let rowNum = 0; rowNum < ROWS; rowNum++) {
-        const row = document.createElement('div');
-        row.style.gridRow = `${gridRowIndex} / span ${ROW_SPAN}`;
-        gridRowIndex += ROW_SPAN;
-        box.appendChild(row);
+    for (let box of boxes) {
+        let gridRowIndex = 1;
+        for (let rowNum = 0; rowNum < nRows; rowNum++) {
+            const row = document.createElement('div');
+            row.style.gridRow = `${gridRowIndex} / span ${rowSpan}`;
+            gridRowIndex += rowSpan;
+            box.appendChild(row);
+        }
     }
-
 }
+
+gridTheBoxes(14, 2);
+
+const box1 = document.querySelector('div.box:nth-child(1)');
+
+
+const thing1 = document.createElement('div');
+thing1.classList.add('thing');
+thing1.style.backgroundColor = "lightblue";
+thing1.style.gridArea = "1 / 1 / 5 / 38";
+
+box1?.appendChild(thing1);
